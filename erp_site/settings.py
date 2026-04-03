4fe6460da16e1a7bc850e082d41d3c93299b0f91
f-22 <fernandoalexthec@gmail.com>
2026-04-03 17:21:13 -0300

    fix: allowed hosts cloud run

--- erp_site/settings.py
+++ erp_site/settings.py
@@ -10,7 +10,11 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-sua-chave-secreta-aqui-par
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['ws-ftthec.onrender.com', 'localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = [
+      'erp-django-app-811970467107.southamerica-east1.run.app',
+    'localhost',
+    '127.0.0.1'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
